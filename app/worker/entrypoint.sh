e3e2930e1b01142c890bf1769f1ee16fe3daaa33
miend <michael.endsley@proton.me>
2024-08-01 01:42:57 -0500

    Update worker dockerfile from alpine -> debian and add podman
    Alpine suffers from a number of very painful issues due to its musl
    version of the standard C library. Using podman for daemonless container
    running (and probably using rootless docker kit as well) surfaces some
    of that pain fairly quickly. The regular deno image is based on debian,
    which is a lot easier to work with.
    
    Podman is added via entrypoint.sh so we can just run its API service and
    avoid needing a docker daemon to talk to -- scaling can occur just by
    scaling the worker container, because it's doing all the heavy lifting
    in its own environment. This also has some security benefits, since docker
    daemons necessarily run as root on their hosts. It also allows Fly.io,
    which doesn't leverage a docker daemon (although it accepts container
    images) to run the worker without any complicated architecture.

---
+++ app/worker/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Run podman's API service in the background, making a docker-compatible socket available
+podman system service --time=0 unix:///var/run/docker.sock &
+
+# Run the metapage worker, with a queue ID provided by environment variable
+deno run \
+    --allow-sys \
+    --allow-net \
+    --allow-read \
+    --allow-write \
+    --allow-env \
+    --allow-run \
+    src/cli.ts \
+    run \
+    --cores=2 \
+    $METAPAGE_QUEUE_ID
