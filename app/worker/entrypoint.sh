e9f95971e860983643f0d66cad885090864f6e3a
miend <michael.endsley@proton.me>
2024-09-06 01:42:48 -0500

    Extract cli.ts flags from entrypoint to allow the user to manipulate them, with sane defaults if no command is supplied when running the container.
    An inelegant solution handling env vars at the entrypoint level instead
    of in the app.

--- app/worker/entrypoint.sh
+++ app/worker/entrypoint.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
 
-# Run podman's API service in the background, making a docker-compatible socket available
-podman system service --time=0 unix:///var/run/docker.sock &
+if [ "$METAPAGE_WORKER_RUN_STANDALONE" = "true" ]; then
+    # Run podman's API service in the background, making a docker-compatible socket available
+    podman system service --time=0 unix:///var/run/docker.sock &
+fi
 
-# Run the metapage worker, with a queue ID provided by environment variable
+if [ -z "$METAPAGE_WORKER_CPUS" ]; then
+    # Default to 1 CPU
+    METAPAGE_WORKER_CPUS=1
+fi
+
+if [ -z "$@" ]; then
+    # Run the metapage worker, with a queue ID provided by environment variable
+    CMD="run --cpus $METAPAGE_WORKER_CPUS $METAPAGE_QUEUE_ID"
+else
+    # Run the command provided by the user
+    CMD="$@"
+fi
+
 deno run \
     --allow-sys \
     --allow-net \
@@ -12,6 +26,4 @@ deno run \
     --allow-env \
     --allow-run \
     src/cli.ts \
-    run \
-    --cores=$METAPAGE_WORKER_CORES \
-    $METAPAGE_QUEUE_ID
+    $CMD
