fddf931af41ef0edcf8bdfc1cfa2433e645ce56c
miend <michael.endsley@proton.me>
2024-11-13 14:53:00 -0600

    Add dynamic opentelemetry scrape targets to pick up all worker groups for metric reporting
    Allow users to specify worker groups as a list of object, meaning they
    can extend with new groups as needed
    
    Add DNS configuration so the load balancer can expose each group by name
    at *.worker.internal

--- app/deploy/gcp/variables.tf
+++ app/deploy/gcp/variables.tf
@@ -17,3 +17,12 @@ variable "worker_image" {
   type        = string
   description = "Address of the container image to use for the worker"
 }
+
+variable "worker_groups" {
+  type = map(object({
+    queue_id      = string
+    instance_type = string
+    cpus          = number
+  }))
+  description = "Definitions of autoscaling worker groups to create. Map key will be used as the name of the worker group, so make sure it's DNS-friendly (no spaces, incompatible special characters, etc.)"
+}
