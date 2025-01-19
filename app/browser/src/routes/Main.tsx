924ccff1445ad7f3009d0673203ce3f21ad57f3e
Dion Amago <dion@transition9.com>
2025-01-19 11:21:32 -0800

    Show help links if queue or local worker not connected

--- app/browser/src/routes/Main.tsx
+++ app/browser/src/routes/Main.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Container, HStack, useMediaQuery, VStack } from "@chakra-ui/react";
+import { Alert, AlertIcon, Box, Container, HStack, Link, useMediaQuery, VStack } from "@chakra-ui/react";
 
 import { useStore } from "../store";
 
@@ -13,8 +13,11 @@ import { PanelSettings } from "/@/components/sections/PanelSettings";
 import { JobControlButton } from "/@/components/header/JobControlButton";
 import { JobStatus } from "/@/components/footer/JobStatus";
 import { PanelDocs } from "/@/components/sections/PanelDocs";
+import { useHashParam } from "@metapages/hash-query/react-hooks";
 
 export const Main: React.FC = () => {
+  const [queueOrUrl] = useHashParam("queue");
+  const isServerConnected = useStore(state => state.isServerConnected);
   const rightPanelContext = useStore(state => state.rightPanelContext);
   const [isWiderThan1000] = useMediaQuery("(min-width: 1000px)");
   const [isTallerThan200] = useMediaQuery("(min-height: 200px)");
@@ -61,7 +64,29 @@ export const Main: React.FC = () => {
       <MainHeader />
       <HStack gap={0} w={"100%"} minW="100vw" minH={"contentHeight"}>
         <Box minW={leftWidth} minH={"contentHeight"}>
-          <PanelLogs mode={stdErrShown ? "stdout" : "stdout+stderr"} />
+          {!isServerConnected ? (
+            <Box minW={leftWidth} minH={"contentHeight"}>
+              {queueOrUrl === "local" ? (
+                <Alert status="error">
+                  <AlertIcon />
+                  The local worker agent is not connected 👉 &nbsp;{" "}
+                  <Link isExternal href="https://docs.metapage.io/docs/container-local-mode">
+                    https://docs.metapage.io/docs/container-local-mode
+                  </Link>
+                </Alert>
+              ) : (
+                <Alert status="error">
+                  <AlertIcon />
+                  No queue set 👉 &nbsp;{" "}
+                  <Link isExternal href="https://docs.metapage.io/docs/container-remote-mode">
+                    https://docs.metapage.io/docs/container-remote-mode
+                  </Link>
+                </Alert>
+              )}
+            </Box>
+          ) : (
+            <PanelLogs mode={stdErrShown ? "stdout" : "stdout+stderr"} />
+          )}
         </Box>
         <Box minW={rightWidth} minH={"contentHeight"} borderLeft={rightContent && "1px"}>
           {rightContent}
