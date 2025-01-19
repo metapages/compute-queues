96ef1e14b4e5f42ed6f058525e3990c1ae799faf
Michael Endsley <michael.endsley@proton.me>
2025-01-19 13:58:41 -0600

    Update naming of Metapage resources and variables to follow new convention

--- app/worker/entrypoint.sh
+++ app/worker/entrypoint.sh
@@ -4,34 +4,34 @@ set -eo pipefail
 
 export API_SERVER_ADDRESS="https://container.mtfm.io"
 
-if [ "$METAPAGE_WORKER_RUN_STANDALONE" = "true" ]; then
+if [ "$METAPAGE_IO_WORKER_RUN_STANDALONE" = "true" ]; then
     # Run docker daemon for environments that don't provide access to one
     dockerd -p /var/run/docker.pid &
 fi
 
-if [ -z "$METAPAGE_WORKER_CPUS" ]; then
+if [ -z "$METAPAGE_IO_WORKER_CPUS" ]; then
     # Default to 1 CPU
-    METAPAGE_WORKER_CPUS=1
+    METAPAGE_IO_WORKER_CPUS=1
 fi
 
-if [ -z "$METAPAGE_WORKER_GPUS" ]; then
+if [ -z "$METAPAGE_IO_WORKER_GPUS" ]; then
     # Default to 0 GPUs
-    METAPAGE_WORKER_GPUS=0
+    METAPAGE_IO_WORKER_GPUS=0
 else
     # Running ldconfig here helps make nvidia libraries available to containers in
     # environments where they're mounted as part of initialization we don't control
     ldconfig
 fi
 
-if [ -n "$METAPAGE_GENERATE_WORKER_ID" ]; then
+if [ -n "$METAPAGE_IO_GENERATE_WORKER_ID" ]; then
     # Generate a random worker ID
-    METAPAGE_WORKER_ID_OPTION="--id $(uuidgen)"
+    METAPAGE_IO_WORKER_ID_OPTION="--id $(uuidgen)"
 fi
 
 if [ -z "$@" ]; then
     # Run the metapage worker, with a queue ID provided by environment variable
     # Will supply the worker ID as well, if it was generated
-    CMD="run --cpus $METAPAGE_WORKER_CPUS --gpus $METAPAGE_WORKER_GPUS $METAPAGE_WORKER_ID_OPTION $METAPAGE_QUEUE_ID"
+    CMD="run --cpus $METAPAGE_IO_WORKER_CPUS --gpus $METAPAGE_IO_WORKER_GPUS $METAPAGE_IO_WORKER_ID_OPTION $METAPAGE_IO_QUEUE_ID"
 else
     # Run the command provided by the user
     CMD="$@"
