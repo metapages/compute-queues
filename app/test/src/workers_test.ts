dc73d6d223ea75b694d77772e8eff6906b606629
Dion Whitehead <260221+dionjwa@users.noreply.github.com>
2025-07-08 10:36:32 -0700

    checkpoint

--- app/test/src/workers_test.ts
+++ app/test/src/workers_test.ts
@@ -108,7 +108,9 @@ Deno.test(
 
     await open(socket);
 
-    // Submit the job
+    // Submit the job. Job submissson should confirm the job is submitted.
+    // Browser clients kinda do this already by resubmitting if the job is
+    // not on the results.
     while (!jobSuccessfullySubmitted) {
       socket.send(JSON.stringify(message));
       await new Promise((resolve) => setTimeout(resolve, 1000));
